6f13f97619d1f8a6119350dde76a5c9b0231b7d3
Kimminguk-kor <65965280+Kimminguk-kor@users.noreply.github.com>
2021-01-08 10:23:31 +0900

    adjustUI

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react"
+// import "../styles/App.css";
+import Main from "../components/Main"
+// import Nav from "../components/Nav"
+
+function App() {
+  return (
+    <div>123
+    {/* <Nav></Nav> */}
+    <Main></Main>
+    </div>
+  );
+}
+
+export default App;
