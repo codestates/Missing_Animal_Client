b1d7a108b3b5cb919794fe6eaacbf5b7c02a57d2
charlie-lyc <ofetwo@gmail.com>
2021-01-20 13:53:30 +0900

    fix-branches

--- src/components/petDetail/PetDetail.jsx
+++ src/components/petDetail/PetDetail.jsx
@@ -1,24 +1,28 @@
 import React from 'react';
 import './petDetail.css';
 
-function PetDetail({ title, description, species, sex, missingDate, area, reward, createdAt, petsImages, contact }) {
+function PetDetail({ title, petname, thumbnail, description, petsImages, species, sex, missingDate, area, reward, username, email, contact, createdAt }) {
     return (
         <div className="petDetail">
             <p className="petDetailTitle">{title}</p>
             <div className="petDetailHeader">
-                <img src={petsImages[0]} alt="petImage1"></img>
-                <img src={petsImages[1]} alt="petImage2"></img>
-                <img src={petsImages[2]} alt="petImage3"></img>
+                {
+                    petsImages.map((image, idx) => 
+                        <img key={idx+1} className= "petsImage" src={image.imagePath} alt={"petImage"+(idx+1)}></img>
+                    )
+                }
             </div>
             <div className="petDetailBody">
-                <p>{species}</p>
-                <p>{sex}</p>
-                <p>{missingDate}</p>
-                <p>{area}</p>
-                <p>{description}</p>
-                <p>{reward}</p>
-                <p>{contact}</p>
-                <p>{createdAt}</p>
+                <p>Species      : {species}</p>
+                <p>Sex          : {sex}</p>
+                <p>Missing Date : {missingDate}</p>
+                <p>Missing Area : {area}</p>
+                <p>Descrioption : {description}</p>
+                <p>Reward       : {reward}</p>
+                <p>Owner's Name : {username}</p>
+                <p>Email        : {email}</p>
+                <p>Contact      : {contact}</p>
+                <p>Created At   : {createdAt}</p>
             </div>
         </div>
     );
